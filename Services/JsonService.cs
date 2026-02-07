3754c754ed69332f7670e8b5457fc4dfb10586f5
kalariashubh <kalariashubh@gmail.com>
2026-02-07 17:27:40 +0530

    Updated JsonService.cs

--- Services/JsonService.cs
+++ Services/JsonService.cs
@@ -1,12 +1,25 @@
-﻿using Newtonsoft.Json;
+using Newtonsoft.Json;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace hatch_automation.Services
 {
+    public class AggregatedLineData
+    {
+        public double Length { get; set; }
+        public int Quantity { get; set; }
+    }
+
+    public class DirectionalOutput
+    {
+        public List<AggregatedLineData> Horizontal { get; set; }
+        public List<AggregatedLineData> Vertical { get; set; }
+    }
+
     public static class JsonService
     {
-        public static void Export(List<LineData> lines)
+        public static void Export(List<LineData> horizontal, List<LineData> vertical)
         {
             string folder = @"D:\Buniyad Byte\POC 2\HATCH AUTOMATION";
 
@@ -15,10 +28,32 @@ namespace hatch_automation.Services
 
             string path = Path.Combine(folder, "line_lengths.json");
 
+            var output = new DirectionalOutput
+            {
+                Horizontal = Aggregate(horizontal),
+                Vertical = Aggregate(vertical)
+            };
+
             string json =
-                JsonConvert.SerializeObject(lines, Formatting.Indented);
+                JsonConvert.SerializeObject(output, Formatting.Indented);
 
             File.WriteAllText(path, json);
+        }
+
+        private static List<AggregatedLineData> Aggregate(List<LineData> lines)
+        {
+            if (lines == null || lines.Count == 0)
+                return null;
+
+            return lines
+                .GroupBy(l => System.Math.Round(l.Length, 2))
+                .Select(g => new AggregatedLineData
+                {
+                    Length = g.Key,
+                    Quantity = g.Count()
+                })
+                .OrderBy(x => x.Length)
+                .ToList();
         }
     }
 }
